f16f0ae55bd0c39358d444f4e7b9114d506ce314
Jorge Banegas <banegas392@gmail.com>
2022-05-10 19:33:54 -0400

    Agregada validacion de fecha

--- src/api/validators/HorarioValidator.js
+++ src/api/validators/HorarioValidator.js
@@ -2,13 +2,21 @@ const {check} = require('express-validator');
 const validateResult = require('../helpers/ValidateHelper');
 
 const validateCreate = [
-    check('fecha_hora').exists().withMessage('La fecha y hora es requerida'),
+    check('fecha_hora').exists().withMessage('La fecha y hora es requerida')
+    .custom(value => {
+        let fecha = new Date(value);
+        let hoy = new Date();
+        if(fecha < hoy){
+            throw new Error('La fecha y hora no es valida, Debe ingresar una fecha y hora mayor a la actual');
+        }
+        return true;
+    }),
     check('idubicacion').exists().withMessage('La ubicacion es requerida'),
 
     (req, res, next) => {
         validateResult(req, res, next);
     }
-]
+];
 
 module.exports = {
     validateCreate
