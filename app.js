bc59f90dff5bd0290badd3b08169a5fcb1de604b
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2022-05-29 15:54:06 -0400

    Se configuro swagger para la documentacion de la API

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+//Swagger configuration
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerConfig = require('./src/config/documentation/swaggerConfig');
+
+
 
 const EventoRoute = require('./src/api/routes/EventoRoutes');
 const CategoriaRoutes = require('./src/api/routes/CategoriaRoutes');
@@ -18,6 +24,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+//swagger middleware
+console.log(swaggerConfig);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(swaggerConfig)));
 
 app.use(API_URI, EventoRoute);
 app.use(API_URI, CategoriaRoutes);
---
+++ src/config/documentation/swaggerConfig.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+//Swagger configuration
+const swaggerSpec = {
+    definition: {
+        openapi : '3.0.0',
+        info: {
+            title: 'Evento API',
+            version: '1.0.0',
+            description: 'API para el manejo de eventos',
+        },
+        servers: [
+            {
+                url: 'http://localhost:4000/api/v1.0.0',
+                description: 'Local server'
+            },
+            {
+                url: 'https://api-eticket.herokuapp.com/api/v1.0.0',
+                description: 'Heroku server'
+            }
+        ]
+    },
+
+    apis: [`${path.join(__dirname, '../src/api/routes/*.js')}`]
+}
+module.exports = swaggerSpec;
