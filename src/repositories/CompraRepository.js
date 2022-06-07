8cb6c5edb7475750f5baf8682f6bb28712f2d930
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2022-06-07 10:47:02 -0400

    Se agrego validacion para la cantidad de entradas

--- src/repositories/CompraRepository.js
+++ src/repositories/CompraRepository.js
@@ -4,22 +4,72 @@ const Ticket = require('../models/Ticket');
 const Detalle_venta = require('../models/Detalle_venta');
 const Tipo_pago = require('../models/Tipo_pago');
 const Datos_cliente = require('../models/Datos_cliente');
-
-const Entradas_sector = require('../models/Entradas_sector');
-const Entradas_ubicacion = require('../models/Entradas_ubicacion');
 const Espacio_reservado = require('../models/Espacio_reservado');
-
 const Ubicacion_repository = require('../repositories/UbicacionRepository');
-const _ubicacion_reporsitory = new Ubicacion_repository();
+const _ubicacion_repository = new Ubicacion_repository();
+
+const Sector_repository = require('../repositories/SectorRepository');
+const _sector_repository = new Sector_repository();
+
+
 class CompraRepository extends BaseRepository {
     constructor() {
         super(Nota_venta);
     }
 
     async entradasDisponiblesUbica(idhorario, idubicacion) {
-        const entradas_vendidas = await _ubicacion_repository.getEntradasDisponibles(idhorario, idubicacion);
-        const total_entradas = await _ubicacion_repository.getCantidadDePersonas(idubicacion);
-        return total_entradas - entradas_vendidas;
+        try {
+            const entradas_vendidas = await _ubicacion_repository.getEntradasDisponibles(idhorario, idubicacion);
+            const total_entradas = await _ubicacion_repository.getCantidadDePersonas(idubicacion);
+            return total_entradas - entradas_vendidas;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async entradasDisponiblesSector(idhorario, idsector) {
+        try {
+            const sector = await _sector_repository.getSectorById(idsector);
+            if (!sector) throw { status: 404, message: 'Sector no encontrado' };
+            const entradas_vendidas = await _sector_repository.getEntradasDisponibles(idhorario, idsector);
+            const total_entradas = await _sector_repository.getCantidadDePersonas(idsector);
+            return {
+                nombre: sector.nombre,
+                entradas_disponibles: total_entradas - entradas_vendidas,
+            };
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async verificarEspacio(idhorario, idespacio) {
+        try {
+            const espacio = await _ubicacion_repository.getEspacioById(idespacio);
+            if (!espacio) throw { status: 404, message: 'Espacio no encontrado' };
+            const espacioReservado = Espacio_reservado.findOne({
+                where: {
+                    idespacio,
+                    idhorario,
+                },
+            });
+            if (espacioReservado) {
+                return {
+                    identificador: espacio.identificador,
+                    disponible: false
+                };
+            }
+            return {
+                identificador: espacio.identificador,
+                disponible: true
+            };
+
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+
     }
 }
 
--- src/services/CompraServices.js
+++ src/services/CompraServices.js
@@ -11,9 +11,12 @@ class CompraService extends BaseServices {
 
     async Comprar(datosUsuario, datosCompra) {
         try {
-            const verificado = await this._verificarIds(datosCompra.idubicacion, datosCompra.idhorario, datosCompra.idevento);
-            if (verificado) {
-                return { message: 'Ids verficaados' };
+            const Datosverificados = await this._verificarIds(datosCompra.idubicacion, datosCompra.idhorario, datosCompra.idevento);
+            if (Datosverificados) {
+                console.log('Ids verficaados');
+                this._verificarEntradas(datosCompra);
+                console.log('TODO OK, Entradas verificadas');
+
             }
 
             throw { status: 404, message: 'Los datos de evento, ubicacion y horario no coinciden o no existen' };
@@ -40,6 +43,39 @@ class CompraService extends BaseServices {
             throw e;
         }
     }
+
+    async _verificarEntradas(datosCompra) {
+        //Si no tiene sectores solo verifica que haya entradas disponibles en la ubicacion
+        if (!datosCompra.sectores) {
+            if (datosCompra.cantidad <= 0) throw { status: 400, message: 'La cantidad de entradas no puede ser menor o igual a 0' };
+            const entradas_disponible_ubicacion = await this._compraRepository.entradasDisponiblesUbica(datosCompra.idhorario, datosCompra.idubicacion);
+            if (entradas_disponible_ubicacion < datosCompra.cantidad) throw { status: 404, message: `Solo quedan ${entradas_disponible_ubicacion} entradas en esta ubicacion` };
+        } else {
+            //Verifica si los sectores tiene espacios, si no tienen solo verifica que haya entradas disponibles en el sector
+            for (let i = 0; i < datosCompra.sectores.length; i++) {
+                if (!datosCompra.sectores[i].espacios) {
+                    if (datosCompra.sectores[i].cantidad <= 0) throw { status: 400, message: 'La cantidad de entradas no puede ser menor o igual a 0' };
+                    const sector = await this._compraRepository.entradasDisponiblesSector(datosCompra.idhorario, datosCompra.sectores[i].idsector);
+                    if (sector.entradas_disponibles < datosCompra.sectores[i].cantidad) throw { status: 404, message: `Solo quedan ${entradas_disponible_sector} entradas en el sector ${sector.nombre}` };
+                } else {
+                    //Si el sector tiene espacios, verifica que estos espacios esten disponibles
+                    for (let j = 0; j < datosCompra.sectores[i].espacios.length; j++) {
+                        const espacio = await this._compraRepository.verificarEspacio(datosCompra.idhorario, datosCompra.sectores[i].espacios[j].idespacio);
+                        let espaciosNoDisponibles = [];
+                        if (!espacio.disponible) {
+                            espaciosNoDisponibles.push(espacio.identificador);
+                        }
+                    }
+                    if (espaciosNoDisponibles.length > 0) {
+                        const espacios = espaciosNoDisponibles.toString();
+                        throw { status: 404, message: `Los espacios ${espacios} no estan disponible` };
+                    }
+
+                }
+            }
+        }
+    }
+
 }
 
 module.exports = CompraService;
