59b7e721a98e677968cebd4a9c1f28331dc77c99
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2022-06-07 21:02:08 -0400

    Se agrego la funcion para compra entradas con  sector

--- src/repositories/CompraRepository.js
+++ src/repositories/CompraRepository.js
@@ -17,6 +17,7 @@ const Sector = require('../models/Sector');
 const Evento = require('../models/Evento');
 const Ubicacion = require('../models/Ubicacion');
 const Entradas_ubicacion = require('../models/Entradas_ubicacion');
+const Entradas_sector = require('../models/Entradas_sector');
 const sequelize = require('../config/database/database');
 const _espacio_repository = new Espacio_repository();
 class CompraRepository extends BaseRepository {
@@ -99,6 +100,108 @@ class CompraRepository extends BaseRepository {
                 await this._compraUbicacion(datosUsuario, iduser, t, datosCompra);
             } else {
                 console.log('Se compra con sector');
+                const datosCliente = await Datos_cliente.create({
+                    nombres: datosUsuario.nombres,
+                    apellidos: datosUsuario.apellidos,
+                    telefono: datosUsuario.telefono,
+                    email: datosUsuario.email,
+                    idusuario: iduser,
+                }, { transaction: t });
+
+                const nota_venta = await Nota_venta.create({
+                    fecha_emision: new Date(),
+                    precio_total: 0,
+                    idusuario: iduser,
+                    idcliente: datosCliente.idcliente,
+                    idpago: datosCompra.tipoPago,
+                    idevento: datosCompra.idevento,
+                }, { transaction: t });
+
+                let listaDetalles = [];
+                let listaIdSectores = [];
+                for (let i = 0; i < verificaSector.length; i++) {
+                    console.log("entre: " + 1);
+                    if (!verificaSector[i].espacios) {
+                        //await this._compraSector(datosUsuario, iduser, t, datosCompra, verificaSector[i]);
+                        let evento = await Evento.findByPk(datosCompra.idevento);
+                        let sector = await Sector.findByPk(verificaSector[i].idsector);
+                        let detalle_venta = await Detalle_venta.create({
+                            descripcion: "Entrada/s para el evento " + evento.nombre + " Sector " + sector.nombre,
+                            cantidad: verificaSector[i].cantidad,
+                            precio_unitario: sector.precio,
+                            importe: sector.precio * verificaSector[i].cantidad,
+                            nronota: nota_venta.nronota,
+                        }, { transaction: t });
+
+                        listaIdSectores.push(verificaSector[i].idsector);
+                        listaDetalles.push(detalle_venta.dataValues);
+                    } else {
+                        for (let j = 0; j < verificaSector[i].espacios.length; j++) {
+                            //await this._compraEspacio(datosUsuario, iduser, t, datosCompra, verificaSector[i], verificaSector[i].espacios[j]);
+                        }
+                    }
+                }
+                console.log("Lista de detalles", listaDetalles);
+                //Modifcar cantidad de entradas disponibles en ubicacion y sector
+                let entradas_total = 0;
+                for (let i = 0; i < listaDetalles.length; i++) {
+                    let entradas_sector = await Entradas_sector.findOne({
+                        where: {
+                            idsector: listaIdSectores[i],
+                            idhorario: datosCompra.idhorario
+                        }
+                    });
+                    if (entradas_sector) {
+                        entradas_sector.cantidad_disponible = entradas_sector.cantidad_vendida - listaDetalles[i].cantidad;
+                        await entradas_sector.save({ transaction: t });
+                        console.log("entradas existia perro: ", entradas_sector);
+                    } else {
+                        let entradas = await Entradas_sector.create({
+                            idsector: listaIdSectores[i],
+                            idhorario: datosCompra.idhorario,
+                            cantidad_vendida: listaDetalles[i].cantidad,
+                        }, { transaction: t });
+                        console.log("entradas no existia perro: ", entradas);
+                    }
+
+                    entradas_total = entradas_total + listaDetalles[i].cantidad;
+                }
+
+                //modificar cantidad de entradas disponibles en ubicacion
+                const entradas_ubicacion = await Entradas_ubicacion.findOne({
+                    where: {
+                        idubicacion: datosCompra.idubicacion,
+                        idhorario: datosCompra.idhorario,
+                    }
+                });
+                if (entradas_ubicacion) {
+                    entradas_ubicacion.entradas_vendidas = entradas_ubicacion.entradas_vendidas + entradas_total;
+                    await entradas_ubicacion.save({ transaction: t });
+                } else {
+                    const entradas = await Entradas_ubicacion.create({
+                        idubicacion: datosCompra.idubicacion,
+                        idhorario: datosCompra.idhorario,
+                        cantidad_vendida: entradas_total,
+                    }, { transaction: t });
+                }
+
+                //generar tickets 
+                let listaTickets = [];
+                for (let i = 0; i < listaDetalles.length; i++) {
+                    for (let j = 0; j < listaDetalles[i].cantidad; j++) {
+                        let ticket = await Ticket.create({
+                            idhorario: datosCompra.idhorario,
+                            idubicacion: datosCompra.idubicacion,
+                            idsector: listaIdSectores[i],
+                            nrodetalle: listaDetalles[i].nrodetalle,
+
+                        }, { transaction: t });
+                        listaTickets.push(ticket.dataValues);
+                    }
+                }
+
+                console.log("Lista de tickets perro: ", listaTickets);
+                throw { status: 200, message: 'Compra realizada con exito' };
             }
         } catch (error) {
             await t.rollback();
