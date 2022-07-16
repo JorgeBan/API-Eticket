0024c5400602ba5db44773950513760cd7be38eb
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2022-07-16 14:52:18 -0400

    fix: se arreglo el error al enviar mails

--- src/config/services/mailConfig.js
+++ src/config/services/mailConfig.js
@@ -75,7 +75,8 @@ function getTemplatePDF(name) {
 }
 
 function getTemplate(name, token) {
-    let url = process.env.URL_DEV + "" || process.env.URL_PRO + "";
+    //let url = process.env.URL_DEV + "" || process.env.URL_PRO + "";
+    let url = process.env.URL_PRO + "";
     console.log("entro a getTemplate",);
     console.log("Entro al template: ", url + "/api/auth/confirm/${token}")
     return `
